bf03a81cf3caef56ff54cc2d1ba178053f51bb33
Vinícios Grama <vinyirun1@gmail.com>
2019-05-24 16:22:47 -0300

    Icone de adicionar aprimorado

--- src/components/AddIcon.js
+++ src/components/AddIcon.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
+import React from 'react';
+import { View, TouchableOpacity, Image } from 'react-native';
+import { } from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default class AddIcon extends Component {
-    render() {
-      return (
-  
-        <View style={{ flexDirection: 'row' }}>
-  
+const AddIcon = () => (
+  <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity onPress={() => this.props.navigationProps.navigate('AdicionarRoupa')}>
-  
             <Image
               source={{uri: 'https://image.flaticon.com/icons/png/512/14/14866.png'}}
               style={{ width: 25, height: 25, marginRight:8, tintColor: '#2699FB' }}
             />
+          </TouchableOpacity> 
+  </View>
+)
+
+// export default AddIcon;
+// export default class AddIcon extends Component {
+//     render() {
+//       return (
   
-          </TouchableOpacity>
+        
   
-        </View>
-  
-      );
+//       );
   
   
-    }
-  }
+//     }
+//   }
