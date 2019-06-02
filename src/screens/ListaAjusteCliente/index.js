61cb6537d4678cd6a1676885179a5f6b224e99e4
Vinícios Grama <vinyirun1@gmail.com>
2019-06-02 18:01:39 -0300

    Lista de ajustes com preços para clientes implementado.

--- src/screens/ListaAjusteCliente/index.js
+++ src/screens/ListaAjusteCliente/index.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
-import { Text, View, ActivityIndicator, SectionList, Button, ScrollView } from 'react-native'
+import { Text, View, ActivityIndicator, SectionList, Button, ScrollView, StyleSheet } from 'react-native'
 import api from '../../services/api';
 
-export default class ListaAjusteEmpresa extends Component {
+export default class ListaAjusteCliente extends Component {
     
     state = {
-        errorMessage: '',
+        errorMessage: null,
         loading: true,
         listaRoupas: [],
-        listaAjustes: []
     }
     
     componentDidMount(){
@@ -17,51 +16,64 @@ export default class ListaAjusteEmpresa extends Component {
 
     buscaLista = async() => {
         try{
-           const res = await api.get('/tipoRoupa/listagem');
-           const resp = await api.get('/preco/listagemPF');
-           this.setState({ listaRoupas: res.data, listaAjustes: resp.data, loading: false });
+           const res = await api.get('/preco/listaPF');
+           this.setState({ listaRoupas: res.data, loading: false });
         } catch(err){
             this.setState({ errorMessage: err.data.error });
         }
-   
     }
 
     render() {
-        const { loading ,listaRoupas } = this.state;
+        const { loading, listaRoupas } = this.state;
         return (   
             <ScrollView>
-                {!this.state.errorMessage && <Text style={{ color: '#fff'}}>{this.state.errorMessage}</Text>}
-                {/* {console.tron.log(this.state)} */}
-                
+                {this.state.errorMessage && <Text style={{ color: 'red'}}>{this.state.errorMessage}</Text>}             
                 { loading ? <ActivityIndicator /> : 
                     <SectionList 
-                    renderItem={({item, index, section}) => <Text key={index}>{item}</Text>}
-                    renderSectionHeader={({section: {title}}) => (
-                      <Text style={{fontWeight: 'bold'}}>{title}</Text>
+                    renderItem={({item, index, section}) => (
+                        <View style={styles.viewAjustes} key={index}>
+                            <Text style={styles.textAjustes}>{item.nometipoajuste}</Text>
+                            <Text style={styles.textAjustes}>{item.preco}</Text>
+                        </View>
                     )}
-                    sections={[
-                      {title: 'Title1', data: ['item1', 'item2']},
-                      {title: 'Title2', data: ['item3', 'item4']},
-                      {title: 'Title3', data: ['item5', 'item6']},
-                    ]}
+                    renderSectionHeader={({section: {title}}) => (
+                        <View style={styles.viewTitle}>
+                            <Text style={styles.title}>{title}</Text>
+                        </View> 
+                    )}
+                    sections={listaRoupas}
                     keyExtractor={(item, index) => item + index}
                     />
-                }
-
-               
-                
-                
-                
+                }               
             </ScrollView>
         )
     }
 }
 
-
-// { this.state.listaAjustes.map(roupa => (
-//     <View key={roupa.id} style={{ marginTop: 15, backgroundColor: 'green' }}>
-//         {/* <Text style={{ fontWeight: 'bold' }}>{project.title}</Text> */}
-//         <Text>{roupa.nometipoajuste}</Text>
-//         <Text>{roupa.preco}</Text>
-//     </View>
-// ))}
+const styles = StyleSheet.create({
+    viewTitle: {
+        backgroundColor: '#11AFE6',
+        width: '100%',
+        height: 40,
+        justifyContent: 'center',
+        paddingLeft: 5,
+        marginBottom: 10
+    },
+    title: {
+        fontFamily: 'OpenSans-Semibold',
+        textTransform: 'uppercase',
+        fontSize: 18
+    },
+    viewAjustes: {
+        width: '97%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 8,
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    textAjustes: {
+        fontFamily: 'OpenSans-Regular',
+        fontSize: 15
+    }
+});
