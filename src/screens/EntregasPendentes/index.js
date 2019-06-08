fdb6128f2892f5b41dbeba4178e9ff53b10685f7
Vinícios Grama <vinyirun1@gmail.com>
2019-06-08 13:22:03 -0300

    Finalização tela entregas pendentes

--- src/screens/EntregasPendentes/index.js
+++ src/screens/EntregasPendentes/index.js
@@ -4,13 +4,20 @@ import {
   Text,
   View,
   FlatList,
-  StyleSheet,
+  ActivityIndicator,
   TouchableOpacity,
+  RefreshControl,
 } from 'react-native';
+
+
+import date from 'date-fns/difference_in_days';
+import formatData from 'date-fns/format';
+import styles from './styles';
+
 
 import api from '../../services/api';
 
-import { StyledEntregas, TextTitulo, TextPrazo } from '../styles/styled';
+import { TextTitulo, TextPrazo } from '../../styles/styled';
 
 export default class EntregasPendentes extends Component {
   state ={
@@ -24,52 +31,86 @@ export default class EntregasPendentes extends Component {
     this.buscaBanco();
   }
 
+  componentWillUnmount() {
+    this.buscaBanco();
+    this.state.roupas === [];
+  }
+
   buscaBanco = async () => {
     try {
       const res = await api.get('/roupa/listagemNaoEntregues');
-      this.setState({ roupa: res.data, loading: false });
+      this.setState({ roupas: res.data, loading: false });
     } catch (err) {
       this.setState({ err: err.data.err, loading: false });
     }
+  }
+
+  onRefresh = () => {
+    this.setState({ refreshing: true });
+    this.buscaBanco().then(() => this.setState({ refreshing: false }));
+  }
+
+  renderData = (data) => {
+    const resp = date(
+      data,
+      Date(),
+    );
+
+    const dataformat = formatData(
+      data,
+      'DD/MM/YYYY',
+    );
+
+
+    if (resp <= 3) {
+      return (
+        <Text style={{ color: 'red' }}>{dataformat}</Text>
+      );
+    }
+    return <Text>{dataformat}</Text>;
   }
 
 
   render() {
     return (
       <View>
-        <FlatList
-          data={data.filter(
-            roupa => roupa.estado === 'CONCLUÍDO' && roupa.entrega === false,
-          )}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              onPress={() => this.props.navigation.navigate('Details', { item })
+        { this.state.loading ? <ActivityIndicator />
+          : (
+            <FlatList
+              data={this.state.roupas}
+              keyExtractor={item => `${item.idroupa}`}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.navigate('DetalhesEntrega', { roupaId: item.idroupa })
               }
-            >
-              <StyledEntregas>
-                <View style={{ flex: 1 }}>
-                  <TextTitulo>{item.name}</TextTitulo>
-                  <Text>{item.cliente}</Text>
-                </View>
-                <View
-                  style={{
-                    flex: 1,
-                    marginBottom: 'auto',
-                    alignItems: 'flex-end',
-                  }}
                 >
-                  <TextPrazo>Prazo</TextPrazo>
-                  {item.prazo === '15/04/19' ? (
-                    <Text style={{ color: 'red' }}>{item.prazo}</Text>
-                  ) : (
-                    <Text>{item.prazo}</Text>
-                  )}
-                </View>
-              </StyledEntregas>
-            </TouchableOpacity>
-          )}
-        />
+                  <View style={styles.roupaTouchable}>
+                    <View style={{ flex: 1 }}>
+                      <TextTitulo>{item.nomeroupa}</TextTitulo>
+                      <Text>{item.nomecliente}</Text>
+                    </View>
+                    <View
+                      style={{
+                        flex: 1,
+                        marginBottom: 'auto',
+                        alignItems: 'flex-end',
+                      }}
+                    >
+                      <TextPrazo>Prazo</TextPrazo>
+                      {this.renderData(item.dataprevista)}
+                    </View>
+                  </View>
+                </TouchableOpacity>
+              )}
+              refreshControl={(
+                <RefreshControl
+                  refreshing={this.state.refreshing}
+                  onRefresh={() => this.onRefresh()}
+                />
+              )}
+            />
+          )
+      }
       </View>
     );
   }
